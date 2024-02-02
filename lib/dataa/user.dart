d7471f5bb40f76781b47f70c578d94857655eb83
Dipak Singh <140516704+DipakSingh0@users.noreply.github.com>
2024-02-02 13:36:39 +0545

    Add files via upload

---
+++ lib/dataa/user.dart
@@ -0,0 +1,30 @@
+// class user{
+//   final String gender;
+//   final String email;
+//   final String phone;
+//   final String cell;
+//   final String name;
+
+// }
+// User({
+//   required this.gender,
+//   required this.email,
+//   required this.phone,
+//   required this.cell,
+//   required this.name,
+
+// });
+//  class UserName {
+
+//   final String title;
+//   final String first;
+//   final String last;
+
+
+//   UserName({
+//   required this.,
+//   required this.  ,
+//   required this.  ,
+  
+//   )};
+// }
